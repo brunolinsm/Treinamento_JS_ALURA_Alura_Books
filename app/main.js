d4efd6ade63d962938511582d74154bb0304c9bc
Bruno Lins Maia <brunolinsm@gmail.com>
2024-03-12 17:38:15 -0300

    Final capitulo 02

--- app/main.js
+++ app/main.js
@@ -4,26 +4,9 @@ const endpointDaAPI =
 
 getBuscarLivrosDaAPI();
 
-const elementoParaInserirLivros = document.getElementById("livros");
-
 async function getBuscarLivrosDaAPI() {
   const res = await fetch(endpointDaAPI);
   livros = await res.json();
-  console.table(livros);
-  exibirLivrosNaTela(livros);
-}
-
-function exibirLivrosNaTela(listaDeLivros) {
-  listaDeLivros.forEach((livro) => {
-    const livroDiv = `
-        <div class="livro">
-        <img src="${livro.imagem}" alt="${livro.alt}" />
-        <h2>${livro.titulo}</h2>
-        <p>Autor: ${livro.autor}</p>
-        <p>Preço: ${livro.preco}</p>
-        <p>Categoria: ${livro.categoria}</span>
-        </div>
-        `;
-    elementoParaInserirLivros.innerHTML += livroDiv;
-  });
+  let livrosComDesconto = aplicarDesconto(livros);
+  exibirLivrosNaTela(livrosComDesconto);
 }
---
+++ app/metodoForEach.js
@@ -0,0 +1,16 @@
+const elementoParaInserirLivros = document.getElementById("livros");
+
+function exibirLivrosNaTela(listaDeLivros) {
+  listaDeLivros.forEach((livro) => {
+    const livroDiv = `
+          <div class="livro">
+          <img src="${livro.imagem}" alt="${livro.alt}" />
+          <h2>${livro.titulo}</h2>
+          <p>Autor: ${livro.autor}</p>
+          <p>Preço: ${livro.preco.toFixed(2)}</p>
+          <p>Categoria: ${livro.categoria}</span>
+          </div>
+          `;
+    elementoParaInserirLivros.innerHTML += livroDiv;
+  });
+}
