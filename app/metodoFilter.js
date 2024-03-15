c61455cc5800b0b1da025dbb56a7eacf771955e6
Bruno Lins Maia <brunolinsm@gmail.com>
2024-03-15 13:06:32 -0300

    Como funciona o filter

---
+++ app/metodoFilter.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorAll(".btn");
+
+botoes.forEach((btn) => btn.addEventListener("click", filtrarLivros));
+
+function filtrarLivros() {
+  const elementBtn = document.getElementById(this.id);
+  const categoria = elementBtn.value;
+  let livrosFiltrados = livros.filter((livro) => livro.categoria == categoria);
+  console.table(livrosFiltrados);
+}
