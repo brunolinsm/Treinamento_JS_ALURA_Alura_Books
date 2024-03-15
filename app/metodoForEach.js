8b05793b225d3019abbb768f9a239d5416fe664c
Bruno Lins Maia <brunolinsm@gmail.com>
2024-03-15 17:52:54 -0300

    04.livros indisponiveis

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,9 +3,13 @@ const elementoParaInserirLivros = document.getElementById("livros");
 function exibirLivrosNaTela(listaDeLivros) {
   elementoParaInserirLivros.innerHTML = "";
   listaDeLivros.forEach((livro) => {
+    let disponibilidade =
+      livro.quantidade > 0 ? "livro__imagens" : "livros__imagens indisponivel";
     const livroDiv = `
           <div class="livro">
-          <img src="${livro.imagem}" alt="${livro.alt}" />
+          <img class="${disponibilidade}" src="${livro.imagem}" alt="${
+      livro.alt
+    }" />
           <h2>${livro.titulo}</h2>
           <p>Autor: ${livro.autor}</p>
           <p>Preço: ${livro.preco.toFixed(2)}</p>
